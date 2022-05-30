5c0691490819517fe89d6b96a394a5b6c4f9c10d
Letícia Mirelly <bbpt0112@bbp.com.br>
2022-05-30 12:01:13 -0300

    feat: added tailwind framework

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-4xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
